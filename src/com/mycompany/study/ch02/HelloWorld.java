5fab19e3a0b3b8ff2a4c29916801b3840f99937f
RyuGyoungBin <fbrudqls@gmail.com>
2023-05-24 15:38:12 +0900

    f.2

---
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.mycompany.study.ch02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HelloWorld");
+		System.out.println("류경빈");
+		System.out.println("남자");
+		System.out.println("10087");
+		System.out.println("101");
+		System.out.println("호");
+		
+	}
+
+}
