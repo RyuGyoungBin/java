d5d977a26b17d7acc2e74c93fe0637272a47073b
RyuGyoungBin <fbrudqls@gmail.com>
2023-05-24 17:33:46 +0900

    f.4

---
+++ src/com/mycompany/study/ch02/Exc0524.java
@@ -0,0 +1,18 @@
+package com.mycompany.study.ch02;
+
+public class Exc0524 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("안녕");
+		
+		int a;
+		a = 1;
+		System.out.println("a : " + a);
+		
+		boolean t = true;
+		System.out.println("t : " + t);
+	}
+
+}
--- src/com/mycompany/study/ch02/HelloWorld.java
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -11,6 +11,24 @@ public class HelloWorld {
 		System.out.println("10087");
 		System.out.println("101");
 		
+		//변수 선언 : 데이터 타입 변수명
+		//변수 초기화 : 데이터타입 변수명 = 리터널
+		
+		boolean a = true;
+		//boolean b = false;
+		
+		int c = 1;
+		System.out.println("c : " + c);
+		System.out.println("a : " + a);
+		
+		int d;
+		d = 10;
+		
+		System.out.println("d : " + d);
+		
+		//int e,f,g;
+		//e = 14;
+		//System.out.println("e : " + e);
 	}
 
 }
